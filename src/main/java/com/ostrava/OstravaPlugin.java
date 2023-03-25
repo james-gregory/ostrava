c2567593f4b6be8d05bc2e55eb27c994e4bd9486
James <james.gregory94@hotmail.co.uk>
2023-03-25 14:50:44 +0000

    Renaming example files to ostrava

--- src/main/java/com/ostrava/OstravaPlugin.java
+++ src/main/java/com/ostrava/OstravaPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.ostrava;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Ostrava"
 )
-public class ExamplePlugin extends Plugin
+public class OstravaPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private OstravaConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	OstravaConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(OstravaConfig.class);
 	}
 }
